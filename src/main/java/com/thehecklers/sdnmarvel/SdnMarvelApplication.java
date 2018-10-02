cd1a74a2f8f65ec8a4656d989641f63e295df662
Jennifer Reif <jennifer.reif@neo4j.com>
2018-10-02 16:09:32 -0500

    Initial commit

---
+++ src/main/java/com/thehecklers/sdnmarvel/SdnMarvelApplication.java
@@ -0,0 +1,12 @@
+package com.thehecklers.sdnmarvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdnMarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdnMarvelApplication.class, args);
+	}
+}
